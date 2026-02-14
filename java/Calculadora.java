e29bdbba1df00478a3654c6f2d83f6fc31c09e71
alba-golpe <golpe.alba@gmail.com>
2026-02-14 21:20:50 +0100

    Subo documentación Java e PHP

---
+++ java/Calculadora.java
@@ -0,0 +1,16 @@
+/**
+ * Clase que realiza operacións matemáticas básicas.
+ */
+public class Calculadora {
+
+    /**
+     * Suma dous valores enteiros.
+     *
+     * @param a primeiro número
+     * @param b segundo número
+     * @return suma de a e b
+     */
+    public int sumar(int a, int b) {
+        return a + b;
+    }
+}
